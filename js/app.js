5e014194afb700cacf1d55a8c6ee35d078ff5825
bz-888 <benjamin.zheng95@gmail.com>
2023-09-27 15:20:22 -0500

    completed exercise 1

--- js/app.js
+++ js/app.js
@@ -49,3 +49,25 @@ playerOne = getPlayerUserName('Luke');
 playerTwo = getPlayerUserName('Laura');
 playerThree = getPlayerUserName('Kate');
 playerFour = getPlayerUserName('Kevin');
+
+
+
+// js-functions-lab
+
+// 1.
+function maxOfTwoNumbers(x, y) {
+    if (x >= y) {
+      return x;
+    } else {
+      return y;
+    }
+    
+    // or more "elegantly" using the fantastic ternary expression!
+    // return  x >= y ? x : y;
+  }
+  
+  console.log(maxOfTwoNumbers(3, 9));
+  
+  // 2.
+
+  
