f670d503006fc8c0b21a91b332b2a69e8831e8aa
bz-888 <benjamin.zheng95@gmail.com>
2023-09-27 14:30:15 -0500

    learning how functions work

--- js/app.js
+++ js/app.js
@@ -1,1 +1,23 @@
 console.log("This is the console output of js-functions-lab.");
+
+// defining a function
+//declaring a function
+// creating the reusable block of code
+// to be used at a later time in the program
+function sayHello (personsName) {
+    // write the code to say hello here
+    console.log(`Hello ${personsName}`);
+};
+
+// execute the fucntion
+// invoke the function
+// call the function
+
+//write the function's name ()
+sayHello("Ben"); // runs the block of code defined by the sayHello function
+
+function sayGoodbye(phrase, name) {
+    console.log(`${phrase}, ${name}!`);
+};
+
+sayGoodbye("Zai jian", "Jim");
