3506b23c47ed3d6cad5a9506c1eb332e17bc8568
bz-888 <benjamin.zheng95@gmail.com>
2023-09-27 14:56:45 -0500

    learned about return

--- js/app.js
+++ js/app.js
@@ -16,8 +16,36 @@ function sayHello (personsName) {
 //write the function's name ()
 sayHello("Ben"); // runs the block of code defined by the sayHello function
 
+
+// create function sayGoodbye with two parameters, one for phrase (e.g. Adios) and one for name (e.g. Bob)
 function sayGoodbye(phrase, name) {
     console.log(`${phrase}, ${name}!`);
 };
 
 sayGoodbye("Zai jian", "Jim");
+
+// function to add two numbers
+function add(x, y) {
+    return x + y; // return produces a value for the function when its is called
+};
+
+console.log(add(2, 2));
+
+
+// function to return usernames in a multiplayer game
+
+function getPlayerUserName(username) {
+    return username.toUpperCase();
+};
+
+// before the game we don't know the players name
+let playerOne;
+let playerTwo;
+let playerThree;
+let playerFour;
+
+// after they join the game, we want to set the player's name
+playerOne = getPlayerUserName('Luke');
+playerTwo = getPlayerUserName('Laura');
+playerThree = getPlayerUserName('Kate');
+playerFour = getPlayerUserName('Kevin');
